1f0cd46a4cece1ad399329c75e8b8c2fe928220e
JunJun <3219030654@stu.cpu.edu.cn>
2025-04-26 11:48:34 +0800

    update subset_data

--- R/subset-data.R
+++ R/subset-data.R
@@ -2,38 +2,37 @@
 #' Subset Data in `serp` and `ribotrans` Objects
 #'
 #' @description
-#' A generic method to subset `serp` and `ribotrans` objects. This function filters internal data
-#' slots (`bam_file`, `library`, `summary_info`) based on user-defined criteria.
+#' A generic method to subset data contained within `serp` and `ribotrans` objects. This function filters internal data slots,
+#' including `bam_file`, `library`, `summary_info`, and if present, `reads_offset_info`, based on user-specified logical conditions.
 #'
 #' @param object An object of class `serp` or `ribotrans`.
-#' @param ... Logical conditions used for subsetting data frames inside the object.
-#' These are passed to the standard `subset()` function.
+#' @param ... Logical expressions passed to the internal `subset()` functions to filter data frames inside the object.
 #'
-#' @return An updated object of class `serp` or `ribotrans`, with internal slots filtered
-#' according to the given criteria.
+#' @return An updated `serp` or `ribotrans` object with internal data slots subsetted according to the specified criteria.
 #'
 #' @details
-#' For `ribotrans` objects, this method:
+#' For `ribotrans` objects, the method:
 #' \itemize{
-#'   \item Subsets the `bam_file` data frame based on `...`.
-#'   \item Subsets the `library` data frame based on `...`.
-#'   \item Filters `summary_info` to include only rows where `sample` exists in the filtered `library`.
+#'   \item Subsets the `bam_file` data frame according to the filtering conditions.
+#'   \item Subsets the `library` data frame according to the filtering conditions.
+#'   \item Filters `summary_info` to retain only rows where the `sample` column is present in the filtered `library`.
+#'   \item If the slot `reads_offset_info` contains rows, it is subsetted similarly by matching `sample` values in the filtered `library`.
 #' }
 #'
-#' For `serp` objects, this method:
+#' For `serp` objects, the method:
 #' \itemize{
-#'   \item Subsets the `bam_file` data frame based on `...`.
-#'   \item Subsets the `library` data frame based on `...`.
-#'   \item Filters `summary_info` where the `sample` column matches
-#'   `paste(lib$sample, lib$type, sep = "-")`, typically used for compound sample IDs.
+#'   \item Subsets the `bam_file` data frame according to the filtering conditions.
+#'   \item Subsets the `library` data frame accordingly.
+#'   \item Filters `summary_info` where the `sample` column matches the compound sample IDs formed by concatenating `lib$sample` and `lib$type` with a hyphen (`paste(lib$sample, lib$type, sep = "-")`).
+#'   \item If `reads_offset_info` is present and non-empty, it is subsetted by samples in the filtered `library`.
 #' }
 #'
 #' @examples
 #' \dontrun{
-#' # Subset a ribotrans object to include only a specific sample
+#' # Subset a ribotrans object to include only samples named "SampleA"
 #' subset_data(ribo, sample == "SampleA")
 #'
-#' # Subset a serp object based on type in library
+#' # Subset a serp object to keep only 'ip' type samples
 #' subset_data(serp_obj, type == "ip")
 #' }
 #'
@@ -56,6 +55,11 @@ setMethod("subset_data",
             sry <- subset(object@summary_info, sample %in% lib$sample)
             object@summary_info <- sry
 
+            if(nrow(object@reads_offset_info) > 0){
+              offset <- subset(object@reads_offset_info, sample %in% lib$sample)
+              object@reads_offset_info <- offset
+            }
+
             return(object)
           }
 )
@@ -75,6 +79,11 @@ setMethod("subset_data",
 
             sry <- subset(object@summary_info, sample %in% paste(lib$sample, lib$type,sep = "-"))
             object@summary_info <- sry
+
+            if(nrow(object@reads_offset_info) > 0){
+              offset <- subset(object@reads_offset_info, sample %in% lib$sample)
+              object@reads_offset_info <- offset
+            }
 
             return(object)
           }
