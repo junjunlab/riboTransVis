93987c0e3d28b63f2255a5d4b56ea487daf6f4fa
junjunlab <1138976957@qq.com>
2025-12-22 15:55:53 +0800

    Update y-axis scaling from 10^5 to 10^6 in plots
    Changed all relevant ggplot layers and y-axis labels in visualization-QC.R to scale read counts by 10^6 instead of 10^5. This provides a more appropriate unit for datasets with larger read counts and ensures consistency across all QC visualizations.

--- R/visualization-QC.R
+++ R/visualization-QC.R
@@ -398,7 +398,7 @@ setMethod("length_plot",
                   dplyr::rename(sample = sample_group)
               }
 
-              ptlayer <- geom_col(aes(x = qwidth, y = counts),width = col_width,fill = col_color)
+              ptlayer <- geom_col(aes(x = qwidth, y = counts/10^6),width = col_width,fill = col_color)
               periodicity.layer <- NULL
               col <- NULL
             }else{
@@ -428,7 +428,7 @@ setMethod("length_plot",
                   dplyr::rename(sample = sample_group)
               }
 
-              ptlayer <- geom_col(aes(x = qwidth, y = counts/10^5, fill = factor(frame)),
+              ptlayer <- geom_col(aes(x = qwidth, y = counts/10^6, fill = factor(frame)),
                                   width = col_width, position = position_dodge2())
 
               # whether add periodicity info
@@ -464,7 +464,7 @@ setMethod("length_plot",
                     axis.text = element_text(colour = "black")) +
               facet +
               # scale_y_continuous(labels = scales::label_log(base = 2,digits = 1)) +
-              xlab("Read length (nt)") + ylab("Number of reads (10^5)")
+              xlab("Read length (nt)") + ylab("Number of reads (10^6)")
 
             # return
             if(return_data == FALSE){
@@ -549,13 +549,13 @@ setMethod("feature_plot",
 
             p <-
               ggplot(sry) +
-              geom_col(aes(x = type,y = counts/10^5,fill = type),width = 0.6,show.legend = F) +
+              geom_col(aes(x = type,y = counts/10^6,fill = type),width = 0.6,show.legend = F) +
               facet +
               theme(panel.grid = element_blank(),
                     strip.text = element_text(colour = "black",face = "bold",size = rel(1)),
                     axis.text = element_text(colour = "black")) +
               scale_y_continuous(labels = scales::label_log(base = 2,digits = 1)) +
-              xlab("Read features") + ylab("Number of reads (10^5)")
+              xlab("Read features") + ylab("Number of reads (10^6)")
 
             # return
             if(return_data == FALSE){
@@ -844,7 +844,7 @@ setMethod("relative_dist_plot",
             # plot
             p <-
               ggplot(pltdf) +
-              geom_col(aes(x = rel, y = counts/10^5, fill = factor(frame)),
+              geom_col(aes(x = rel, y = counts/10^6, fill = factor(frame)),
                        width = 0.9, position = position_dodge2()) +
               theme(panel.grid = element_blank(),
                     strip.text = element_text(colour = "black",face = "bold",size = rel(1)),
@@ -854,7 +854,7 @@ setMethod("relative_dist_plot",
               # scale_y_continuous(labels = scales::label_log(base = 10,digits = 1)) +
               scale_fill_manual(values = c("0" = "#003366", "1" = "#336699", "2" = "#CCCCCC"),
                                 name = "frame") +
-              xlab("Distance to start/stop codon (nt)") + ylab("Number of reads (10^5)")
+              xlab("Distance to start/stop codon (nt)") + ylab("Number of reads (10^6)")
 
             # return
             if(return_data == FALSE){
@@ -1141,7 +1141,7 @@ setMethod("relative_offset_plot",
             # plot
             p <-
               ggplot(summary.info) +
-              geom_path(aes(x = rel_pos,y = counts/10^5)) +
+              geom_path(aes(x = rel_pos,y = counts/10^6)) +
               mx.label +
               geom_vline(xintercept = xpos,lty ="dashed", color = "orange") +
               ggh4x::facet_grid2(qwidth~sample,scales = "free",independent = "y",axes = "y") +
@@ -1153,7 +1153,7 @@ setMethod("relative_offset_plot",
               scale_fill_viridis_c(option = "mako") +
               # scale_y_continuous(labels = scales::label_log(base = 10,digits = 1)) +
               xlab(xlab) +
-              ylab("Number of reads length (10^5)")
+              ylab("Number of reads length (10^6)")
 
 
             # check return type
